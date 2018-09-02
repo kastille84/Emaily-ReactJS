c1900e9d5dbab33cd7945bfcc1c4f2cbc522e180
Edwin Martinez <kastille84@gmail.com>
2018-09-02 13:27:22 -0400

    added requireLogin middleware

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
 require('./routes/billingRoutes')(app);
 
 
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,17 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
+
         const charge = await stripe.charges.create({
                     amount: 500,
                     currency: 'usd',
                     description: '$5 for 5 credits',
                     source: req.body.id //*important* this is the token give to use by stripe on frontend
                 });
+        //passport assigns the user model to our request as req.user
         //this user prop is the same user in our DB, therefore we can call save on it to update our record in Db
         req.user.credits += 5;
         const user = await req.user.save();      
